7b9c904ff86319ea16038c6201b8f4574cd87765
Franklin "Snaipe" Mathieu <me@snai.pe>
2017-06-18 19:51:41 +0100

    docs: added some documentation

--- pager.go
+++ pager.go
@@ -20,6 +20,12 @@ var (
 	ErrClosed    = errors.New("The pager was closed.")
 )
 
+// Pager spawns and lets users write to a pager program.
+//
+// If an error occurs while writing to the pager, all subsequent writes,
+// Close, and Error, will return that error.
+//
+// It implements io.WriteCloser.
 type Pager struct {
 	proc *exec.Cmd
 	out  io.Writer
@@ -30,10 +36,20 @@ type flusher interface {
 	Flush() error
 }
 
+// Open calls OpenPager("", nil).
 func Open() (*Pager, error) {
 	return OpenPager("", nil)
 }
 
+// OpenPager spawns the pager program from the passed command, redirecting its
+// standard output to dst, and returns a new Pager object.
+//
+// If command is an empty string, then the value of the PAGER enviroment
+// variable is used instead. If it is also empty, then ErrNoCommand is returned.
+// Any command is valid as long as they are acceptable as an operand to
+// "sh -c".
+//
+// If dst is nil, os.Stdout is used instead.
 func OpenPager(command string, dst io.Writer) (*Pager, error) {
 	p := &Pager{}
 
@@ -44,12 +60,12 @@ func OpenPager(command string, dst io.Writer) (*Pager, error) {
 		return nil, ErrNoCommand
 	}
 
-	if out == nil {
-		out = os.Stdout
+	if dst == nil {
+		dst = os.Stdout
 	}
 
 	var err error
-	if fl, ok := out.(flusher); ok {
+	if fl, ok := dst.(flusher); ok {
 		err = fl.Flush()
 		if err != nil {
 			return nil, err
@@ -57,7 +73,7 @@ func OpenPager(command string, dst io.Writer) (*Pager, error) {
 	}
 
 	p.proc = exec.Command("sh", "-c", command)
-	p.proc.Stdout = out
+	p.proc.Stdout = dst
 	p.proc.Stderr = os.Stderr
 	p.out, err = p.proc.StdinPipe()
 	if err != nil {
@@ -101,6 +117,13 @@ func (p *Pager) cleanup() {
 	p.out = nil
 }
 
+// Write writes the contents of data into the pager, and returns the number
+// of bytes written. If n < len(data), it also returns an error explaining
+// why the write is short.
+//
+// If Closed was previously called, or if the user closes the read end of the
+// pager -- typically by exiting the pager program -- then Write returns
+// the ErrClosed error.
 func (p *Pager) Write(data []byte) (n int, err error) {
 	if p.out == nil {
 		return 0, ErrClosed
@@ -117,6 +140,9 @@ func (p *Pager) Write(data []byte) (n int, err error) {
 	return
 }
 
+// Close closes the write end of the pager and frees all ressources associated
+// with it. It returns the last write error that occured, or any error from
+// the pager process.
 func (p *Pager) Close() error {
 	if p.out != nil {
 		p.cleanup()
@@ -124,6 +150,7 @@ func (p *Pager) Close() error {
 	return p.err
 }
 
+// Error returns the last error that occured during a write or Close.
 func (p *Pager) Error() error {
 	return p.err
 }
